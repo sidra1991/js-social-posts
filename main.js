d6994d88aef45e15f5e67867d2845a539e3c3c8e
sidra1991 <1991sidra@gmail.com>
2022-03-12 18:05:20 +0100

    manca il like

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/*
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Nel file js avete un array di oggetti che rappresentano ciascun post.
+Ogni post contiene le informazioni necessarie per stampare la relativa card:
+id del post (numero progressivo da 1 a n),
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+*/
 const posts = [
     {
         "id": 1,
@@ -56,20 +68,74 @@ const posts = [
     }
 ];
 
+
 /*
-Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
-Nel file js avete un array di oggetti che rappresentano ciascun post.
-Ogni post contiene le informazioni necessarie per stampare la relativa card:
-id del post (numero progressivo da 1 a n),
-nome autore,
-foto autore,
-data in formato americano (mm-gg-yyyy),
-testo del post,
-immagine (non tutti i post devono avere una immagine),
-numero di likes.
 Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+*/
+
+const post = document.createElement(`div`);
+post.classList.add("post")
+
+for (let i = 0; i < posts.length; i++) {
+    let autorImmage = ``;
+    let vuoto = '';
+
+    if (posts[i].author.image === null ) {
+       autorImmage = `<span> ${posts[i].author.name[0]} </span> `;
+       vuoto = "profile-pic-default" 
+    } else {
+      autorImmage = `<img class="profile-pic " src= "${posts[i].author.image}" alt="${posts[i].author.name}"> `;
+    }
+
+    post.innerHTML +=
+        `
+    <div class="post__header">
+        <div class="post-meta">                    
+                <div class="post-meta__icon ${vuoto}">
+                ${autorImmage}                 
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts[i].author.name}</div>
+                <div class="post-meta__time">${posts[i].created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${posts[i].content}</div>
+    <div class="post__image">
+        <img src=${posts[i].media} alt="">
+        
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1"  onclick=getLike() id="${posts[i].id} >
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true" ></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+`;
+
+    document.getElementById(`container`).append(post)
+
+};
+
+/*
 Milestone 2 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
-Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+Salviamo in un secondo ar
+*/
+
+function getLike() {
+    document.querySelectorAll(button).classList.add(`.like-button--liked`)
+    
+}
+
+
+/*
 BONUS
 1. Formattare le date in formato italiano (gg/mm/aaaa)
 2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
